12c2261251b7a583458b34fe895db28f6291ae77
Alexandre Syenchuk <as@netica.fr>
2017-04-03 12:48:14 +0200

    Fixed provision.sh execution rights and added some bash aliases

--- provision.sh
+++ provision.sh
@@ -4,6 +4,26 @@
 apt-get update && apt-get upgrade -y
 apt-get install -y --no-install-recommends  apt-utils software-properties-common
 
+# sets up bash profile
+sed -i 's/#force_color_prompt=yes/force_color_prompt=yes/g' /etc/skel/.bashrc
+sed -i 's/#force_color_prompt=yes/force_color_prompt=yes/g' ~/.bashrc
+
+cat > /etc/skel/.bash_aliases << EOF
+alias ls='ls -Fhv --si --color --group-directories-first --time-style=long-iso'
+alias l='ls -l'
+alias ll='ls -la'
+alias llt='ll -t'
+alias dir='l -X'
+alias vdir='l -d'
+alias genpwd="tr -dc [:print:] < /dev/urandom | tr -d '\\\`^' | fold -w 20 | head -n 1 ; tr -dc [:alnum:] < /dev/urandom | fold -w 20 | head -n 1"
+EOF
+mkdir /etc/skel/.ssh
+chmod go-rwx /etc/skel/.ssh/
+
+cp /etc/skel/.bash_aliases ~/
+source ~/.bashrc
+source ~/.bash_aliases
+
 # installs Python 3.6
 add-apt-repository -y ppa:jonathonf/python-3.6
 apt-get update
