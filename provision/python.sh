47e7cc959a4d1daa4a8238867ce67c4a95219239
Alexandre Syenchuk <as@netica.fr>
2017-04-03 19:08:44 +0200

    Added gcc dependency, useful for mysqlclient (Python lib)

--- provision/python.sh
+++ provision/python.sh
@@ -5,8 +5,8 @@ add-apt-repository -y ppa:jonathonf/python-3.6
 apt-get update
 apt-get install -y python3.6 python3.6-dev
 
-# installs general libraries
-apt-get install -y --no-install-recommends libxml2-dev libxslt1-dev libffi-dev libssl-dev libmysqlclient-dev acl
+# installs general libraries, tools
+apt-get install -y --no-install-recommends libxml2-dev libxslt1-dev libffi-dev libssl-dev libmysqlclient-dev acl gcc
 
 # installs python-related tools and libraries
 apt-get install -y --no-install-recommends virtualenv python3-virtualenv python3-pip python3-setuptools python3-dev python3-wheel
