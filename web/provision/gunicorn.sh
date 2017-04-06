89ab8e4213212c2f32be49c15134eb5641932601
Alexandre Syenchuk <as@netica.fr>
2017-04-06 17:31:26 +0200

    Addd web tag

---
+++ web/provision/gunicorn.sh
@@ -0,0 +1,20 @@
+#! /bin/bash
+
+pip install gunicorn
+
+# configures supervisor for gunicorn
+cat > /etc/supervisor/conf.d/gunicorn.conf << EOF
+[program:gunicorn]
+directory=/app/workdir
+command=/app/code/bin/gunicorn.sh
+user=www-data
+autostart=true
+autorestart=unexpected
+stopasgroup=true
+stdout_logfile=/app/log/gunicorn-stdout.log
+stdout_logfile_maxbytes=50MB
+stdout_logfile_backups=10
+stderr_logfile=/app/log/gunicorn-stderr.log
+stderr_logfile_maxbytes=50MB
+stderr_logfile_backups=10
+EOF
